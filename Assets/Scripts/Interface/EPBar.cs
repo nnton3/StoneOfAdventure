5ba6b435bf4cd3d2b7bfae83d6c8be8983f3d766
nnton3 <akarnomazov@gmail.com>
2018-04-05 16:22:42 +0300

    Revert "pls noo"
    This reverts commit b493f1a7e328ced189fc5fa51555886891c960f9.

---
+++ Assets/Scripts/Interface/EPBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EPBar : ProgressBar {
+
+	/*UnitParams playerParams;
+
+	void Start () {
+		playerParams = GameManager.player.GetComponent<UnitParams> ();
+	}
+
+	void Update ()
+	{
+		if (playerParams.EP != currentPercent) {
+			isOn = true;
+			if (playerParams.EP < currentPercent && Mathf.Abs (playerParams.EP - currentPercent) > 3f) {
+				currentPercent -= speed * Time.deltaTime;
+			} else if (playerParams.EP > currentPercent && Mathf.Abs (playerParams.EP - currentPercent) > 3f) {
+				currentPercent += speed * Time.deltaTime;
+			}
+		} else {
+			isOn = false;
+		}
+		loadingBar.GetComponent<Image> ().fillAmount = currentPercent / 100;
+	}*/
+}
