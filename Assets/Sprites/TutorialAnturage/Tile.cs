2e473cb22965e0e83ab1ca20f6952b43b71325c5
nnton3 <akarnomazov@gmail.com>
2018-08-14 13:25:48 +0300

    test random level system

---
+++ Assets/Sprites/TutorialAnturage/Tile.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+	public Vector2 startPosition;
+	public Vector2 endPosition;
+}
