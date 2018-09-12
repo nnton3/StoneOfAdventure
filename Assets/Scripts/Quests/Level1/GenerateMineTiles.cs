5608c8bf535b7c1aa939cb7e7134be330f26f253
nnton3 <akarnomazov@gmail.com>
2018-09-12 08:54:07 +0300

    кривые тайлы, опять все переделывать самому

---
+++ Assets/Scripts/Quests/Level1/GenerateMineTiles.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateMineTiles : GameLevel {
+
+	public override void GenerateLevel ()
+	{
+		parentObject = GameObject.Find ("MineBG").transform;
+		base.GenerateLevel();
+		parentObject.gameObject.SetActive (false);
+	}
+}
