1402e5c448e8efb6d2e8e65c116617a977fc0146
nnton3 <akarnomazov@gmail.com>
2018-10-11 20:02:47 +0300

    update quest system

---
+++ Assets/Scripts/BaseScripts/QuestScripts/Drop.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drop : MonoBehaviour {
+
+	Conditions conditionsScript;
+
+	void Start () {
+		conditionsScript = GetComponent<Conditions> ();
+	}
+	
+	void Update () {
+		if (!objectsDropped && !conditionsScript.alive) {
+			DroppedObjects ();
+		}
+	}
+
+	bool objectsDropped = false;
+
+	public GameObject[] avaliablaObjects;
+	void DroppedObjects () {
+		objectsDropped = true;
+		for (int i = 0; i < avaliablaObjects.Length; i++) {
+			Instantiate (avaliablaObjects [i], transform);
+		}
+	}
+}
