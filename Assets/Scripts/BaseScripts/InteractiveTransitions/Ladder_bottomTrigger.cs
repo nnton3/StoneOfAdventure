a873b13bce7701aa2ff767d8fb88ad6e6a9cebca
nnton3 <akarnomazov@gmail.com>
2018-10-20 22:10:25 +0300

    add blacksmith home

---
+++ Assets/Scripts/BaseScripts/InteractiveTransitions/Ladder_bottomTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder_bottomTrigger : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D player) {
+		if (player.CompareTag ("Player")) {
+
+		}
+	}
+}
